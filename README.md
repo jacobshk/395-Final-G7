b0349cb738a352fa77dad72083f0eb26eadd52ce
Sean <stran27@gmu.edu>
2024-02-24 23:27:08 -0500

    Added Homepage, Sign-in page, Sign-Up page, some minor logo tweaks, and routing to pages.

---
+++ README.md
@@ -0,0 +1,4 @@
+Resources:
+- Poodle icon was taken from https://icon-icons.com/icon/poodle/100869
+- SignIn, SignUp, and navgiation bar taken from mui:https://mui.com/
+
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            
+            href="/"
+            sx={{ mr: 2 }}
+          >
+            <img src="poodle.ico" width="50" height="50"></img>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Poodle Classroom
+          </Typography>
+          <Button href="/SignUp" color="inherit">Sign Up</Button>
+          <Button href="/SignIn" color="inherit">Sign In</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
